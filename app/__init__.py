47dd2a493287e22cbc50ca0383d28b9065648e08
mmohammedaof857 <mohammedaof17@gmail.com>
2026-09-10 15:50:52 +0530

    Implement health check and Gmail AI agent routes
    Added health check and Gmail AI agent endpoints with error handling.

--- app/__init__.py
+++ app/__init__.py
@@ -15,18 +15,74 @@ from app.youtube import youtube_bp
 def create_app():
 
     app = Flask(__name__)
+    CORS(app)
 
+    #youtube
     app.register_blueprint(
         youtube_bp,
         url_prefix="/youtube"
     )
 
+    #Home
     @app.route("/")
     def home():
         return render_template("index.html")
 
+    #HTML
     @app.route("/html")
     def html():
         return render_template("index.html")
 
-    return app
+    #health
+    app.route("/health")
+    return jsonify({
+        "status": "ok"
+        "service": "Nova AI Agent"
+    })
+
+    #Gmail AI Agent
+    @app.route("/agent", method=["POST"])
+    def agent();
+
+        try: 
+            data = request.get_json(silent=true) or {}
+            command = data.get("command","").strtip()
+
+            if not command:
+                return jsonify({
+                    "success": False,
+                    "message": "command is required"
+                }),400
+
+            if not is_email_command(command):
+                return jsonify({
+                    "success": False,
+                    "message": "please give a gmail command."
+                }),400
+
+    recipient = extract_email(command)
+
+    email = generate_email_with_gemini(command)
+
+    return jsonify({
+        "success": True,
+        "type": "email",
+        "email_generated": True,
+        "recipient": recipient,
+        "subject": email["subject"],
+        "body": email["body"],
+        "gmail_url": create_gmail_url(
+            email["subjects"],
+            email["body"],
+            recipient
+        )
+    })
+
+    except Exception as e:
+
+    return jsonify({
+        "success": False
+        "message": str(0)
+    }), 500
+
+return app
