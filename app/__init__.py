e581f284f6f5bcb1858c848e35a29ec507459515
mmohammedaof857 <mohammedaof17@gmail.com>
2026-09-10 15:00:37 +0530

    Enhance app initialization with CORS and Gmail imports
    Added CORS support and Gmail-related imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
-from app.youtube import youtube_bp
+import os
 
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+from app.gmail import(
+    is_email_command,
+    extract_email,
+    create_gmail,
+    generate_email_with_gemini
+)
+
+from app.youtube import youtube_bp
 
 def create_app():
 
